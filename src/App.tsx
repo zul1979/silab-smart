89bf7f9869b63fdee138939fec8f73c3c1e08b9f
Zulkifli Ahmad <ahmadzulkifli477@gmail.com>
2026-07-14 20:23:38 +0900

    Migrate project to Vite structure

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+function App() {
+    return (
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "#edf2f7",
+        }}
+      >
+        <div
+          style={{
+            background: "white",
+            padding: "40px",
+            borderRadius: "16px",
+            boxShadow: "0 8px 20px rgba(0,0,0,.15)",
+          }}
+        >
+          <h1
+            style={{
+              color: "#008080",
+              marginBottom: "10px",
+            }}
+          >
+            SILAB SMART
+          </h1>
+  
+          <p>Sistem Informasi Laboratorium FKIP Universitas Khairun</p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
